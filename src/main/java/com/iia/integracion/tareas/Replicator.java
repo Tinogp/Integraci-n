51ff7c8eb2d87a367f76c4c2e8ad4cd373eb7324
Enrique Lobo Rubio <kikeloborubio@gmail.com>
2025-11-01 09:39:05 +0100

    Tarea y Replicator

--- src/main/java/com/iia/integracion/tareas/Replicator.java
+++ src/main/java/com/iia/integracion/tareas/Replicator.java
@@ -1,9 +1,21 @@
 package com.iia.integracion.tareas;
 
+import com.iia.integracion.model.slot.Slot;
+import java.util.List;
+
 /**
  * 
- * @author tinog
+ * @author Quique
  */
-public class Replicator {
+public class Replicator extends Tarea{
+
+    public Replicator(List<Slot> entradas, List<Slot> salidas) {
+        super(entradas, salidas);
+    }
+
+    @Override
+    public void ejecuta() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
