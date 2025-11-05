fc7877150dc713f218f09d3b9b5027352acb372e
Enrique Lobo Rubio <kikeloborubio@gmail.com>
2025-11-05 11:05:36 +0100

    Aggregator actualizado

--- src/main/java/com/iia/integracion/tareas/Aggregator.java
+++ src/main/java/com/iia/integracion/tareas/Aggregator.java
@@ -1,9 +1,93 @@
 package com.iia.integracion.tareas;
 
+import com.iia.integracion.model.mensaje.Mensaje;
+import com.iia.integracion.model.slot.Slot;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
 /**
- * 
- * @author tinog
+ *
+ * @author Quique
  */
-public class Aggregator {
-    
+public class Aggregator extends Tarea {
+
+    private Map<UUID, List<Mensaje>> mapaFragmentos; //asocia los fragmentps al mensaje original   
+
+    public Aggregator(List<Slot> entradas, List<Slot> salidas, String xpath) {
+        super(entradas, salidas, xpath);
+
+        this.mapaFragmentos = new HashMap<>();
+    }
+
+    @Override
+    public void ejecuta() {
+        
+        Mensaje msg = entradas.getFirst().leerSlot();
+
+        UUID idMsg = msg.getId();
+
+        // Almacenar el fragmento
+        List<Mensaje> listaFrag = mapaFragmentos.computeIfAbsent(idMsg, k -> new ArrayList<>());
+        listaFrag.add(msg);
+
+        if (listaFrag.size() == msg.getTamano()) {            
+            
+            Document infoCompleta = unirFragmentos(listaFrag, this.xpathExpression, idMsg);
+
+            Mensaje msgFinal = new Mensaje(infoCompleta, idMsg);
+
+            salidas.getFirst().escribirSlot(msgFinal);
+
+            mapaFragmentos.remove(idMsg);
+        }
+    }
+
+    /**
+     * Crea un nuevo documento XML con los fragmentos de la lista.
+     *
+     * @param fragmentos Lista de fragmentos
+     * @param xpath Nombre del elemento raíz del nuevo documento.
+     * @return El Documento XML completo
+     */
+    private Document unirFragmentos(List<Mensaje> fragmentos, String raíz, UUID id) {
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            Document nuevoDoc = builder.newDocument();
+            Node raiz = nuevoDoc.createElement(raíz);
+            nuevoDoc.appendChild(raiz);
+
+            Node orderId = nuevoDoc.createElement("order_id");
+            orderId.setTextContent(id.toString());
+            raiz.appendChild(orderId);
+
+            Node drinks = nuevoDoc.createElement("drinks");
+            raiz.appendChild(drinks);
+
+            for (Mensaje fragmento : fragmentos) {
+                Node raizFrag = fragmento.getCuerpo().getDocumentElement();
+                Node nodoimport = nuevoDoc.importNode(raizFrag, true);
+                drinks.appendChild(nodoimport);
+            }
+
+            return nuevoDoc;
+
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(Aggregator.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
 }
---
+++ src/test/java/com/iia/integracion/tareas/AggregatorTest.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.iia.integracion.tareas;
+
+import com.iia.integracion.model.mensaje.Mensaje;
+import java.util.UUID;
+import com.iia.integracion.model.slot.Slot;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author Enrique
+ */
+public class AggregatorTest {
+    
+    private Slot entrada;
+    private Slot salida;
+    private Aggregator aggregator;   
+    
+    private static final String fragmento1 = "<drink><name>cafe</name><type>hot</type></drink>";
+    private static final String fragmento2 = "<drink><name>coca-cola</name><type>cold</type></drink>";
+    private static final String fragmento3 = "<drink><name>chocolate</name><type>hot</type></drink>";
+    private static final String fragmento4 = "<drink><name>agua</name><type>cold</type></drink>";
+    
+    private static final String raiz = "cafe_order";       
+    
+    @BeforeEach
+    public void setUp() {
+        entrada = new Slot();
+        salida = new Slot();
+        
+        aggregator = new Aggregator(List.of(entrada), List.of(salida), raiz);        
+    }
+      
+    @Test
+    public void testAggregator() {
+        
+        //Crear mensajes y fragmentos      
+        Mensaje msgOriginal = new Mensaje(null); //sin cuerpo solo para id
+        Mensaje msgOriginal2 = new Mensaje(null); //sin cuerpo solo para id     
+        
+        Mensaje msgFrag1 = new Mensaje(crearDocumentoDesdeString(fragmento1), msgOriginal.getId());        
+        msgFrag1.setIdFragment(UUID.randomUUID());
+        msgFrag1.setTamano(2);        
+        Mensaje msgFrag2 = new Mensaje(crearDocumentoDesdeString(fragmento2), msgOriginal.getId());        
+        msgFrag2.setIdFragment(UUID.randomUUID());
+        msgFrag2.setTamano(2);
+        
+        Mensaje msgFrag3 = new Mensaje(crearDocumentoDesdeString(fragmento3), msgOriginal2.getId());
+        msgFrag3.setIdFragment(UUID.randomUUID());
+        msgFrag3.setTamano(2);
+        Mensaje msgFrag4 = new Mensaje(crearDocumentoDesdeString(fragmento4), msgOriginal2.getId());        
+        msgFrag4.setIdFragment(UUID.randomUUID());
+        msgFrag4.setTamano(2);        
+                
+        //Enviar Fragmentos
+        entrada.escribirSlot(msgFrag1);
+        aggregator.ejecuta();                            
+        entrada.escribirSlot(msgFrag3);
+        aggregator.ejecuta();
+        entrada.escribirSlot(msgFrag2);
+        aggregator.ejecuta();  
+        
+        //Comprobaciones
+        assertEquals(1, salida.numMensajes());  
+        Mensaje msgFinal = salida.leerSlot();       
+        
+        System.out.println("\n--- Mensaje Final ---");
+        System.out.println(msgFinal.toString());
+        System.out.println("------------------------------\n");                  
+    }
+    
+    private Document crearDocumentoDesdeString(String xmlString) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            InputSource is = new InputSource(new StringReader(xmlString));
+            return (Document) builder.parse(is);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            throw new RuntimeException("Error al parsear XML de prueba", e);
+        }
+    }
+    
+}
