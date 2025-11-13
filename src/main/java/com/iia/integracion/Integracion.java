d15aa167eb4a4af8c7f0174638757a7d5bf4d306
Enrique Lobo Rubio <kikeloborubio@gmail.com>
2025-11-13 20:43:34 +0100

    Cambio conector de camarero y main

--- src/main/java/com/iia/integracion/Integracion.java
+++ src/main/java/com/iia/integracion/Integracion.java
@@ -1,62 +1,81 @@
 package com.iia.integracion;
 
-import com.iia.integracion.model.conector.ConectorBX;
-import com.iia.integracion.model.conector.ConectorComanda;
-import com.iia.integracion.model.mensaje.Mensaje;
-import com.iia.integracion.model.puerto.PuertoEntrada;
-import com.iia.integracion.model.puerto.PuertoSolicitud;
+import com.iia.integracion.model.conector.*;
+import com.iia.integracion.model.puerto.*;
 import com.iia.integracion.model.slot.Slot;
-import com.iia.integracion.tareas.Distributor;
-import com.iia.integracion.tareas.Replicator;
-import com.iia.integracion.tareas.Splitter;
-import java.util.ArrayList;
+import com.iia.integracion.tareas.*;
+import java.util.List;
 
 public class Integracion {
 
     public static void main(String[] args) {
+
         // Declaracion de Slots
         Slot poll = new Slot();
-        Slot splRep = new Slot();
-        Slot repDisup = new Slot();
-        Slot repDisdown = new Slot();
-        Slot disTransUp = new Slot();
-        Slot disTransDown = new Slot();
-        Slot transbf = new Slot();
-        Slot transbc = new Slot();
-        Slot bfcorr = new Slot();
-        Slot bccorr = new Slot();
+        Slot splDis = new Slot();
+        Slot disRepbf = new Slot();
+        Slot disRepbc = new Slot();
+        Slot repTransbf = new Slot();
+        Slot repTransbc = new Slot();
+        Slot repCorrbf = new Slot();
+        Slot repCorrbc = new Slot();
+        Slot transPuerbf = new Slot(); 
+        Slot transPuerbc = new Slot(); 
+        Slot puerCorrbf = new Slot();
+        Slot puerCorrbc = new Slot();        
+        Slot corrEnbf1 = new Slot();
+        Slot corrEnbf2 = new Slot();
+        Slot corrEnbc1 = new Slot();
+        Slot corrEnbc2 = new Slot();
+        Slot enMerbf = new Slot();
+        Slot enMerbc = new Slot();
+        Slot merAgg = new Slot();
+        Slot salida = new Slot();
 
         // Declaracion de puertos
         PuertoEntrada puertoe = new PuertoEntrada(poll);
-        PuertoSolicitud puertosolbf = new PuertoSolicitud(transbf, bfcorr);
-        PuertoSolicitud puertosolbc = new PuertoSolicitud(transbc, bccorr);
+        PuertoSolicitud puertoSolbf = new PuertoSolicitud(transPuerbf, puerCorrbf);
+        PuertoSolicitud puertoSolbc = new PuertoSolicitud(transPuerbc, puerCorrbc);
+        PuertoSalida puertoSal = new PuertoSalida(salida);
 
         // Declaracion de conectores
-        ConectorComanda comandas = new ConectorComanda(puertoe, ".\\Comanda");
-        ConectorBX bf = new ConectorBX(puertosolbf, "jdbc:mysql://tinovpn.duckdns.org:2025/bebidas");
-        ConectorBX bc = new ConectorBX(puertosolbc, "jdbc:mysql://tinovpn.duckdns.org:2025/bebidas");
+        ConectorComanda comandas = new ConectorComanda(puertoe, ".\\Comandas");
+        ConectorBX bf = new ConectorBX(puertoSolbf, "jdbc:mysql://tinovpn.duckdns.org:2025/bebidas");
+        ConectorBX bc = new ConectorBX(puertoSolbc, "jdbc:mysql://tinovpn.duckdns.org:2025/bebidas");
+        ConectorCamarero camarero = new ConectorCamarero(puertoSal, "/entregas/camarero.xml");
 
 
         // Declaracion de tareas
-        ArrayList<Slot> apoll = new ArrayList<>();
-        apoll.add(poll);
-        ArrayList<Slot> aSplRep = new ArrayList<>();
-        apoll.add(splRep);
-        Splitter spl = new Splitter(apoll, aSplRep, "");
+        Splitter spl = new Splitter(List.of(poll), List.of(splDis), "/cafe_order/drinks/drink");            
+        Distributor dis = new Distributor(List.of(splDis), List.of(disRepbf, disRepbc), List.of("drink/type = 'cold'", "drink/type = 'hot'"));        
+        Replicator repbf = new Replicator(List.of(disRepbf), List.of(repTransbf, repCorrbf));
+        Replicator repbc = new Replicator(List.of(disRepbc), List.of(repTransbc, repCorrbc));
+        Translator transbf = new Translator(List.of(repTransbf), List.of(transPuerbf), "/frio.xsl"); //mirar archivo
+        Translator transbc = new Translator(List.of(repTransbc), List.of(transPuerbc), "/caliente.xsl");
+        Correlator corrbf = new Correlator(List.of(repCorrbf, puerCorrbf), List.of(corrEnbf1, corrEnbf2), "/drink/id");
+        Correlator corrbc = new Correlator(List.of(repCorrbc, puerCorrbc), List.of(corrEnbc1, corrEnbc2), "/drink/id");
+        Enricher enbf = new Enricher(List.of(corrEnbf1, corrEnbf2), List.of(enMerbf));
+        Enricher enbc = new Enricher(List.of(corrEnbc1, corrEnbc2), List.of(enMerbc));
+        Merger mer = new Merger(List.of(enMerbf, enMerbc), List.of(merAgg));
+        Aggregator agg = new Aggregator(List.of(merAgg), List.of(salida), "cafe_order");
 
-        ArrayList<Slot> aRepDis = new ArrayList<>();
-        aRepDis.add(repDisup);
-        aRepDis.add(repDisdown);
-        Replicator rep = new Replicator(aSplRep, aRepDis);
-
-        ArrayList<Slot> aDisTransUp = new ArrayList<>();
-        aDisTransUp.add(disTransUp);
-        ArrayList<Slot> aDisTransDown = new ArrayList<>();
-        aDisTransDown.add(disTransDown);
-        Distributor disUp = new Distributor(aRepDis.subList(0, 0), aDisTransUp, "");
-        Distributor disDown = new Distributor(aRepDis.subList(1, 1), aDisTransDown, "");
-
-        
+        // Ejecucion del sistema de integracion
+        comandas.ejecuta();
+        spl.ejecuta();
+        dis.ejecuta();
+        repbf.ejecuta();
+        repbc.ejecuta();
+        transbf.ejecuta();        
+        transbc.ejecuta();
+        bf.ejecuta();
+        bc.ejecuta();
+        corrbf.ejecuta();
+        corrbc.ejecuta();
+        enbf.ejecuta();
+        enbc.ejecuta();
+        mer.ejecuta();
+        agg.ejecuta();
+        camarero.ejecuta();                          
     }
 
 }
