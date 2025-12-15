926c0e2bb851e2924f7a791686a76841ad05da7b
tinog <tinog@PC_Tino>
2025-12-15 17:35:42 +0100

    Corrigiendo error merge

--- src/main/java/com/iia/integracion/model/concurrencia/politicaPrioridad.java
+++
@@ -1,8 +0,0 @@
-package com.iia.integracion.model.concurrencia;
-
-import java.util.List;
-import com.iia.integracion.tareas.Tarea;
-
-public interface politicaPrioridad {
-    public void asignarPrioridades(List<Tarea> tareas);
-}
